1016c9f5a037e69d751a4992bd38123153d6665a
Freedyx29 <147407587+Freedyx29@users.noreply.github.com>
2024-11-14 17:42:04 +0100

    Update traitement.inscrit.php

--- traitement/traitement.inscrit.php
+++ traitement/traitement.inscrit.php
@@ -1,41 +1,30 @@
 <?php
-include 'class.inscrit.php';
+
+include '../class/class.inscrit.php';
 
 $oinscrit = new Inscrit();
 
 if (isset($_POST['ajouter'])) {
-    $idcours = $_POST['idcours'];
-    $idcava = $_POST['idcava'];
-
-    if (!empty($idcours) && !empty($idcava)) {
-        $success = $oinscrit->InscritAjt($idcours, $idcava) ? 1 : 0;
-        header("Location: vue.inscrit.php?success=$success");
-    } else {
-        echo "Les champs idcours et idcava ne peuvent pas être vides.";
-    }
+    $refidcours = $_POST['idcours'];
+    $refidcava = $_POST['idcava'];
+    $success = $oinscrit->InscritAjt($refidcours, $refidcava) ? 1 : 0;
+    header("Location: ../vue/vue.inscrit.php?success=$success");
 }
-
 
 if (isset($_POST['modifier'])) {
-    $idcours = $_POST['idcours'];
-    $idcava = $_POST['idcava'];
-    $new_idcours = $_POST['new_idcours'];
-    $new_idcava = $_POST['new_idcava'];
-
-    $oinscrit->setidcours($idcours);
-    $oinscrit->setidcava($idcava);
-
-    // Assurez-vous que la modification est réussie
-    $success = $oinscrit->Modifier($new_idcours, $new_idcava) ? 1 : 0;
-    header("Location: vue.inscrit.php?success=$success");
+    $id_cours_first = $_POST['id_cours_first'];
+    $id_cava_first = $_POST['id_cava_first'];
+    $refidcours = $_POST['idcours'];
+    $refidcava = $_POST['idcava'];
+    $success = $oinscrit->Modifier($id_cours_first, $id_cava_first, $refidcours, $refidcava) ? 1 : 0;
+    header("Location: ../vue/vue.inscrit.php?success=$success");
 }
-
-
 
 if (isset($_POST['supprimer'])) {
-    $idcours = $_POST['idcours'];
-    $idcava = $_POST['idcava'];
-    $success = $oinscrit->Supprimer($idcours, $idcava) ? 1 : 0;
-    header("Location: vue.inscrit.php?success=$success");
+    $refidcours = $_POST['refidcours'];
+    $refidcava = $_POST['refidcava'];
+    $success = $oinscrit->Supprimer($refidcours, $refidcava) ? 1 : 0;
+    header("Location: ../vue/vue.inscrit.php?success=$success");
 }
+
 ?>
