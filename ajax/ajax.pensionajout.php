fd9c0958324150319bede5990d8383cbdaaa16b2
Freedyx29 <147407587+Freedyx29@users.noreply.github.com>
2024-11-16 18:20:19 +0100

    Update ajax.pensionajout.php

--- ajax/ajax.pensionajout.php
+++ ajax/ajax.pensionajout.php
@@ -5,7 +5,7 @@ if(isset($_POST['keyword'])) {
 	$con = connexionPDO();
 $keyword = '%'.$_POST['keyword'].'%';
 
-$sql = "SELECT * FROM cavalerie WHERE nomche LIKE (:var) ORDER BY idgalop ASC LIMIT 0, 100";
+$sql = "SELECT * FROM cavalerie WHERE nomche LIKE (:var) ORDER BY numsire ASC LIMIT 0, 100";
 $req = $con->prepare($sql);
 $req->bindParam(':var', $keyword, PDO::PARAM_STR);
 $req->execute();
@@ -13,9 +13,9 @@ $req->execute();
 $list = $req->fetchAll();
 
 foreach ($list as $res) {
-    $Listecavalerieajout = str_replace($_POST['keyword'], '<b>'.$_POST['keyword'].'</b>', $res['nomche']);
+    $Listepensionajout = str_replace($_POST['keyword'], '<b>'.$_POST['keyword'].'</b>', $res['nomche']);
     // Utilise l'index `id` pour l'élément HTML, qui est passé depuis la fonction autocomplet() dans le JavaScript
-    echo '<li onclick="set_item_ajout(\''.str_replace("'", "\'", $res['nomche']).'\',  '.$res['nomche'].')">'.$Listecavalerieajout.'</li>';
+    echo '<li onclick="set_item_pension_ajout(\''.str_replace("'", "\'", $res['nomche']).'\',  '.$res['numsire'].')">'.$Listepensionajout.'</li>';
     }
 }
 ?>
