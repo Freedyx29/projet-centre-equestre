591fa774346ea631db9bf5a3ccbcfa12d897b961
Freedyx29 <147407587+Freedyx29@users.noreply.github.com>
2024-11-16 18:19:11 +0100

    Update script_pension.js

--- js/script_pension.js
+++ js/script_pension.js
@@ -1,61 +1,60 @@
 // autocompletion
-function autocompletNomche(id) {
+function autocompletPension(id) {
     var min_length = 1;
     var keyword = $('#nomche' + id).val();
 
     if (keyword.length >= min_length) {
         $.ajax({
-            url: 'ajax.pension.php', 
+            url: '../ajax/ajax.pension.php', 
             type: 'POST',
             data: { 
                 keyword: keyword,
                 index: id // Envoie l'index à ajax_produit.php
             },
             success: function(data) {
-                $('#nom_list_nomche_id' + id).show();
-                $('#nom_list_nomche_id' + id).html(data);
+                $('#nom_list_pension_id' + id).show();
+                $('#nom_list_pension_id' + id).html(data);
             },
         });
     } else {
-        $('#nom_list_nomche_id' + id).hide();
+        $('#nom_list_pension_id' + id).hide();
     }
 }
 
-function autocompletNomcheajout() {
+function autocompletPensionajout() {
     var min_length = 1;
     var keyword = $('#nomche').val();
 
     if (keyword.length >= min_length) {
         $.ajax({
-            url: 'ajax.pensionajout.php', 
+            url: '../ajax/ajax.pensionajout.php', 
             type: 'POST',
             data: { 
                 keyword: keyword,
-                // Envoie l'index à ajax_produit.php
             },
             success: function(data) {
-                $('#nom_list_nomche_id').show();
-                $('#nom_list_nomche_id').html(data);
+                $('#nom_list_pension_id').show();
+                $('#nom_list_pension_id').html(data);
             },
         });
     } else {
-        $('#nom_list_nomche_id').hide();
+        $('#nom_list_pension_id').hide();
     }
 }
 
 
 // Lors du choix dans la liste
-function set_item(item, id, num_sire) {
+function set_item_pension(item, id, num_sire) {
     // Mettre à jour le champ libgalop avec la valeur sélectionnée
     $('#nomche' + id).val(item);
     // Cacher la liste de suggestions
-    $('#nom_list_nomche_id' + id).hide();
-    // Mettre à jour le champ caché id_galop avec l'ID de la catégorie sélectionnée
+    $('#nom_list_pension_id' + id).hide();
+    // Mettre à jour le champ caché num_sire avec l'ID de la catégorie sélectionnée
     $('#num_sire' + id).val(num_sire);
 }
 
-function set_item_ajout(item, num_sire) {
+function set_item_pension_ajout(item, num_sire) {
     $('#nomche').val(item);
-    $('#nom_list_nomche_id').hide();
+    $('#nom_list_pension_id').hide();
     $('#num_sire').val(num_sire);
 }
