d84132339e49d878ed1b6d0629e2f7ba4e1ffa7f
melikekiris <melikekiris24@gmail.com>
2024-11-18 16:59:37 +0100

    script_pension.js

--- js/script_pension.js
+++ js/script_pension.js
@@ -1,23 +1,23 @@
 // autocompletion
 function autocompletPension(id) {
     var min_length = 1;
-    var keyword = $('#nomche' + id).val();
+    var keyword = $('#modifier_nomche').val();
 
     if (keyword.length >= min_length) {
         $.ajax({
-            url: '../ajax/ajax.pension.php', 
+            url: '../ajax/ajax.pension.php',
             type: 'POST',
-            data: { 
+            data: {
                 keyword: keyword,
                 index: id // Envoie l'index à ajax_produit.php
             },
             success: function(data) {
-                $('#nom_list_pension_id' + id).show();
-                $('#nom_list_pension_id' + id).html(data);
+                $('#modifier_nom_list_pension_id').show();
+                $('#modifier_nom_list_pension_id').html(data);
             },
         });
     } else {
-        $('#nom_list_pension_id' + id).hide();
+        $('#modifier_nom_list_pension_id').hide();
     }
 }
 
@@ -27,9 +27,9 @@ function autocompletPensionajout() {
 
     if (keyword.length >= min_length) {
         $.ajax({
-            url: '../ajax/ajax.pensionajout.php', 
+            url: '../ajax/ajax.pensionajout.php',
             type: 'POST',
-            data: { 
+            data: {
                 keyword: keyword,
             },
             success: function(data) {
@@ -42,15 +42,14 @@ function autocompletPensionajout() {
     }
 }
 
-
 // Lors du choix dans la liste
 function set_item_pension(item, id, num_sire) {
     // Mettre à jour le champ libgalop avec la valeur sélectionnée
-    $('#nomche' + id).val(item);
+    $('#modifier_nomche').val(item);
     // Cacher la liste de suggestions
-    $('#nom_list_pension_id' + id).hide();
+    $('#modifier_nom_list_pension_id').hide();
     // Mettre à jour le champ caché num_sire avec l'ID de la catégorie sélectionnée
-    $('#num_sire' + id).val(num_sire);
+    $('#modifier_num_sire').val(num_sire);
 }
 
 function set_item_pension_ajout(item, num_sire) {
