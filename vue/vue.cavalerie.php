5c72d699edbfcb8efc0c610c14696fd42d2c00d6
melikekiris <melikekiris24@gmail.com>
2024-11-19 09:41:38 +0100

    vue.cavalerie.php

--- vue/vue.cavalerie.php
+++ vue/vue.cavalerie.php
@@ -16,7 +16,6 @@ $cavalerieList = $cavalerie->CavalerieALL();
     <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
     <script src="../js/script_cavalerie.race.js"></script>
     <script src="../js/script_cavalerie.robe.js"></script>
-    <script src="../js/script_cavalerie.js"></script>
 
     <style>
         .form-popup {
