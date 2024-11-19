1d4cbb36b19e5dcfdc52d693e38e9779d1f97ffe
rouan52 <ayarouan52@gmail.com>
2024-11-19 09:44:05 +0100

    Update vue.race.php

--- vue/vue.race.php
+++ vue/vue.race.php
@@ -56,7 +56,7 @@ $reqRace = $oRace->RaceALL(); // Supposons que vous avez une méthode pour obten
     <section class="form-section">
         <h2>Gestion des Races</h2>
         <button onclick="openPopup('addPopup')">Ajouter</button>
-        <button id="editButton" onclick="openPopup('editPopup')" disabled>Modifier La,Race</button>
+        <button id="editButton" onclick="openPopup('editPopup')" disabled>Modifier La Race</button>
         <button id="deleteButton" onclick="openPopup('deletePopup')" disabled>Supprimer La Race</button>
     </section>
 
