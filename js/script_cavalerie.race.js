c6ba48df4b43bc0299f7e8ea50286130f6a7a5a2
melikekiris <melikekiris24@gmail.com>
2024-11-19 08:54:53 +0100

    script_cavalerie.race.js

--- js/script_cavalerie.race.js
+++ js/script_cavalerie.race.js
@@ -1,60 +1,31 @@
-// autocompletion
+// Autocompletion pour la race
 function autocompletRace(id) {
     var min_length = 1;
-    var keyword = $('#librace' + id).val();
+    var keyword = $('#' + id + 'librace').val();
 
     if (keyword.length >= min_length) {
         $.ajax({
-            url: 'ajax.cavalerie.race.php', 
+            url: '../ajax/ajax.cavalerie.race.php',
             type: 'POST',
-            data: { 
+            data: {
                 keyword: keyword,
                 index: id
             },
             success: function(data) {
-                $('#nom_list_race_id' + id).show();
-                $('#nom_list_race_id' + id).html(data);
+                $('#' + id + 'nom_list_race_id').show();
+                $('#' + id + 'nom_list_race_id').html(data);
             },
         });
     } else {
-        $('#nom_list_race_id' + id).hide();
+        $('#' + id + 'nom_list_race_id').hide();
     }
 }
-
-function autocompletRaceajout() {
-    var min_length = 1;
-    var keyword = $('#librace').val();
-
-    if (keyword.length >= min_length) {
-        $.ajax({
-            url: 'ajax.cavalerie.raceajout.php', 
-            type: 'POST',
-            data: { 
-                keyword: keyword,
-            },
-            success: function(data) {
-                $('#nom_list_race_id').show();
-                $('#nom_list_race_id').html(data);
-            },
-        });
-    } else {
-        $('#nom_list_race_id').hide();
-    }
-}
-
 
 // Lors du choix dans la liste
-function set_item(item, id, id_race) {
-    // Mettre à jour le champ librace avec la valeur sélectionnée
-    $('#librace' + id).val(item);
-    // Cacher la liste de suggestions
-    $('#nom_list_race_id' + id).hide();
-    // Mettre à jour le champ caché id_race avec l'ID de la catégorie sélectionnée
-    $('#id_race' + id).val(id_race);
-}
-
-function set_item_ajout(item, id_race) {
-    $('#librace').val(item);
-    $('#nom_list_race_id').hide();
-    $('#id_race').val(id_race);
+function set_item(item, id, id_race, type) {
+    if (type === 'race') {
+        $('#' + id + 'librace').val(item);
+        $('#' + id + 'nom_list_race_id').hide();
+        $('#' + id + 'id_race').val(id_race);
+    }
 }
