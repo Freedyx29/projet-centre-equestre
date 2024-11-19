ba07df1436dc2cc3f1e83e0614008291aadd29a4
Freedyx29 <147407587+Freedyx29@users.noreply.github.com>
2024-11-19 09:25:54 +0100

    Update traitement.cours.php

--- traitement/traitement.cours.php
+++ traitement/traitement.cours.php
@@ -1,38 +1,32 @@
 <?php
-require_once 'class.cours.php';
 
-$cours = new Cours();
+include '../class/class.cours.php';
 
-if (isset($_POST['create'])) {
-    // Vérification des champs
-    $libcours = isset($_POST['libcours']) ? $_POST['libcours'] : null;
-    $hdebut = isset($_POST['hdebut']) ? $_POST['hdebut'] : null;
-    $hfin = isset($_POST['hfin']) ? $_POST['hfin'] : null;
-    $jour = isset($_POST['jour']) ? $_POST['jour'] : null;
+$ocours = new Cours();
 
-    // Vérification si les valeurs sont définies
-    if ($libcours && $hdebut && $hfin && $jour) {
-        $success = $cours->create($libcours, $hdebut, $hfin, $jour) ? 1 : 0;
-        header("Location: vue.cours.php?success=$success");
-    } else {
-        // Gérer le cas où une des valeurs est manquante
-        echo "Erreur : Tous les champs doivent être remplis.";
-    }
+if (isset($_POST['ajouter'])) {
+    $libcours = $_POST['libcours'];
+    $hdebut = $_POST['hdebut'];
+    $hfin = $_POST['hfin'];
+    $jour = $_POST['jour'];
+    $success = $ocours->CoursAjt($libcours, $hdebut, $hfin, $jour) ? 1 : 0 ;
+    header("Location: ../vue/vue.cours.php?success=$success");
 }
 
-if (isset($_POST['update'])) {
+if (isset($_POST['modifier'])) {
     $idcours = $_POST['idcours'];
     $libcours = $_POST['libcours'];
     $hdebut = $_POST['hdebut'];
     $hfin = $_POST['hfin'];
     $jour = $_POST['jour'];
-    $success = $cours->update($idcours, $libcours, $hdebut, $hfin, $jour) ? 1 : 0;
-    header("Location: vue.cours.php?success=$success");
+    $success = $ocours->Modifier($idcours, $libcours, $hdebut, $hfin, $jour) ? 1 : 0 ;
+    header("Location: ../vue/vue.cours.php?success=$success");
 }
 
-if (isset($_POST['delete'])) {
+if (isset($_POST['supprimer'])) {
     $idcours = $_POST['idcours'];
-    $success = $cours->delete($idcours) ? 1 : 0;
-    header("Location: vue.cours.php?success=$success");
+    $success = $ocours->Supprimer($idcours) ? 1 : 0;
+    header("Location: ../vue/vue.cours.php?success=$success");
 }
+
 ?>
