729c84d1646b3d93e56f8ce4f723ccf828080744
rouan52 <ayarouan52@gmail.com>
2024-11-19 11:50:35 +0100

    Update traitement.cavaliers.php

--- traitement/traitement.cavaliers.php
+++ traitement/traitement.cavaliers.php
@@ -1,6 +1,5 @@
 <?php
-
-include 'class.cavaliers.php';
+include 'class.cavaliers.php'; // Assurez-vous que ce chemin est correct
 
 $ocavaliers = new Cavaliers();
 
@@ -17,13 +16,12 @@ if (isset($_POST['ajouter'])) {
     $cpresp = $_POST['cpresp'];
     $telresp = $_POST['telresp'];
     $emailresp = $_POST['emailresp'];
-    $password = $_POST['password'];
+    $password = $_POST['motdepasse'];
     $assurance = $_POST['assurance'];
     $idgalop = $_POST['idgalop'];
-    $success = $ocavaliers->CavaliersAjt($nomcava, $prenomcava, $datenacava, $numlic, $photo,
-                                        $nomresp, $prenomresp, $rueresp, $vilresp, $cpresp, $telresp,
-                                        $emailresp, $password, $assurance, $idgalop ) ? 1 : 0;
-    header("Location: ../vue/vue.cavaliers.php?success=$success");
+    $success = $ocavaliers->CavaliersAjt($nomcava, $prenomcava, $datenacava, $numlic, $photo, $nomresp, $prenomresp, $rueresp, $vilresp, $cpresp, $telresp, $emailresp, $password, $assurance, $idgalop) ? 1 : 0;
+    header("Location: vue.cavaliers.php?success=$success");
+    exit();
 }
 
 if (isset($_POST['modifier'])) {
@@ -40,19 +38,18 @@ if (isset($_POST['modifier'])) {
     $cpresp = $_POST['cpresp'];
     $telresp = $_POST['telresp'];
     $emailresp = $_POST['emailresp'];
-    $password = $_POST['password'];
+    $password = $_POST['motdepasse'];
     $assurance = $_POST['assurance'];
     $idgalop = $_POST['idgalop'];
-    $success = $ocavaliers->Modifier($idcava, $nomcava, $prenomcava, $datenacava, $numlic, $photo,
-    $nomresp, $prenomresp, $rueresp, $vilresp, $cpresp, $telresp,
-    $emailresp, $password, $assurance, $idgalop) ? 1 : 0;
-    header("Location: ../vue/vue.cavaliers.php?success=$success");
+    $success = $ocavaliers->Modifier($idcava, $nomcava, $prenomcava, $datenacava, $numlic, $photo, $nomresp, $prenomresp, $rueresp, $vilresp, $cpresp, $telresp, $emailresp, $password, $assurance, $idgalop) ? 1 : 0;
+    header("Location: vue.cavaliers.php?success=$success");
+    exit();
 }
 
 if (isset($_POST['supprimer'])) {
     $idcava = $_POST['idcava'];
     $success = $ocavaliers->Supprimer($idcava) ? 1 : 0;
-    header("Location: ../vue/vue.cavaliers.php?success=$success");
+    header("Location: vue.cavaliers.php?success=$success");
+    exit();
 }
-
 ?>
