7409454e92303c17ec241a29158c6fc9226b83ee
rouan52 <ayarouan52@gmail.com>
2024-11-19 11:52:12 +0100

    Update ajax.cavaliers.php

--- ajax/ajax.cavaliers.php
+++ ajax/ajax.cavaliers.php
@@ -1,21 +1,20 @@
 <?php
 include '../include/bdd.inc.php';
 
-if(isset($_POST['keyword'])) {
-	$con = connexionPDO();
-$keyword = '%'.$_POST['keyword'].'%';
+if (isset($_POST['keyword'])) {
+    $con = connexionPDO();
+    $keyword = '%'.$_POST['keyword'].'%';
 
-$sql = "SELECT * FROM galop WHERE libgalop LIKE (:var) ORDER BY idgalop ASC LIMIT 0, 100";
-$req = $con->prepare($sql);
-$req->bindParam(':var', $keyword, PDO::PARAM_STR);
-$req->execute();
+    $sql = "SELECT * FROM galop WHERE libgalop LIKE (:var) ORDER BY idgalop ASC LIMIT 0, 100";
+    $req = $con->prepare($sql);
+    $req->bindParam(':var', $keyword, PDO::PARAM_STR);
+    $req->execute();
 
-$list = $req->fetchAll();
+    $list = $req->fetchAll();
 
-foreach ($list as $res) {
-    $Listegalop = str_replace($_POST['keyword'], '<b>'.$_POST['keyword'].'</b>', $res['libgalop']);
-    // Utilise l'index `id` pour l'élément HTML, qui est passé depuis la fonction autocomplet() dans le JavaScript
-    echo '<li onclick="set_item(\''.str_replace("'", "\'", $res['libgalop']).'\', '.$_POST['index'].', '.$res['idgalop'].')">'.$Listegalop.'</li>';
+    foreach ($list as $res) {
+        $Listegalop = str_replace($_POST['keyword'], '<b>'.$_POST['keyword'].'</b>', $res['libgalop']);
+        echo '<li onclick="set_item_cavalier(\''.str_replace("'", "\'", $res['libgalop']).'\', '.$res['idgalop'].')">'.$Listegalop.'</li>';
     }
 }
 ?>
