938affd555e1b28029436531ddab42e2e852e74b
Freedyx29 <147407587+Freedyx29@users.noreply.github.com>
2024-11-24 23:27:31 +0100

    Update traitement.pension.php

--- traitement/traitement.pension.php
+++ traitement/traitement.pension.php
@@ -6,23 +6,32 @@ $opension = new Pension();
 
 if (isset($_POST['ajouter'])) {
     $libpen = $_POST['libpen'];
+    $dateD = $_POST['dateD'];
+    $dateF = $_POST['dateF'];
+    $tarif = $_POST['tarif'];
     $numsire = $_POST['numsire'];
-    $success = $opension->PensionAjt($libpen, $numsire ) ? 1 : 0;
-    header("Location: ../vue/vue.pension.php?success=$success");
+    $success = $opension->PensionAjt($libpen, $dateD, $dateF, $tarif, $numsire) ? 1 : 0;
+    header('Location: ../vue/vue.pension.php?success=1&message=Pension ajoutée avec succès');
+    exit();
 }
 
 if (isset($_POST['modifier'])) {
     $idpen = $_POST['idpen'];
     $libpen = $_POST['libpen'];
+    $dateD = $_POST['dateD'];
+    $dateF = $_POST['dateF'];
+    $tarif = $_POST['tarif'];
     $numsire = $_POST['numsire'];
-    $success = $opension->Modifier($idpen, $libpen, $numsire) ? 1 : 0;
-    header("Location: ../vue/vue.pension.php?success=$success");
+    $success = $opension->Modifier($idpen, $libpen, $dateD, $dateF, $tarif, $numsire) ? 1 : 0;
+    header('Location: ../vue/vue.pension.php?success=1&message=Pension modifiée avec succès');
+    exit();
 }
 
 if (isset($_POST['supprimer'])) {
     $idpen = $_POST['idpen'];
     $success = $opension->Supprimer($idpen) ? 1 : 0;
-    header("Location: ../vue/vue.pension.php?success=$success");
+    header('Location: ../vue/vue.pension.php?success=1&message=Pension supprimée avec succès');
+    exit();
 }
 
 ?>
