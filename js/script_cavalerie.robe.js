3d3961ebd464b8f32901fa61f44b3d716aca22eb
Freedyx29 <147407587+Freedyx29@users.noreply.github.com>
2024-11-25 16:36:26 +0100

    Update script_cavalerie.robe.js

--- js/script_cavalerie.robe.js
+++ js/script_cavalerie.robe.js
@@ -1,7 +1,7 @@
-// Autocompletion pour la robe
+// autocompletion
 function autocompletRobe(id) {
     var min_length = 1;
-    var keyword = $('#' + id + 'librobe').val();
+    var keyword = $('#librobe' + id).val();
 
     if (keyword.length >= min_length) {
         $.ajax({
@@ -9,23 +9,51 @@ function autocompletRobe(id) {
             type: 'POST',
             data: {
                 keyword: keyword,
-                index: id
+                index: id // Envoie l'index à ajax_produit.php
             },
             success: function(data) {
-                $('#' + id + 'nom_list_robe_id').show();
-                $('#' + id + 'nom_list_robe_id').html(data);
+                $('#nom_list_robe_id' + id).show();
+                    $('#nom_list_robe_id' + id).html(data);
             },
         });
     } else {
-        $('#' + id + 'nom_list_robe_id').hide();
+        $('#nom_list_robe_id' + id).hide();
+    }
+}
+
+function autocompletRaceajout() {
+    var min_length = 1;
+    var keyword = $('#librobe').val();
+
+    if (keyword.length >= min_length) {
+        $.ajax({
+            url: '../ajax/ajax.cavalerie.raceajout.php',
+            type: 'POST',
+            data: {
+                keyword: keyword,
+            },
+            success: function(data) {
+                $('#nom_list_robe_id').show();
+                $('#nom_list_robe_id').html(data);
+            },
+        });
+    } else {
+        $('#nom_list_robe_id').hide();
     }
 }
 
 // Lors du choix dans la liste
-function set_item(item, id, id_robe, type) {
-    if (type === 'robe') {
-        $('#' + id + 'librobe').val(item);
-        $('#' + id + 'nom_list_robe_id').hide();
-        $('#' + id + 'id_robe').val(id_robe);
-    }
+function set_item_robe(item, id, id_robe) {
+    // Mettre à jour le champ libgalop avec la valeur sélectionnée
+    $('#librobe' + id).val(item);
+    // Cacher la liste de suggestions
+    $('#nom_list_robe_id' + id).hide();
+    // Mettre à jour le champ caché num_sire avec l'ID de la catégorie sélectionnée
+    $('#id_robe' + id).val(id_robe);
+}
+
+function set_item_robe_ajout(item, id_robe) {
+    $('#librobe').val(item);
+    $('#nom_list_robe_id').hide();
+    $('#id_robe').val(id_robe);
 }
