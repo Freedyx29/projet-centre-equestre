7cfffc4cf929b11c88b77d6a5c677be9b21eb641
Freedyx29 <147407587+Freedyx29@users.noreply.github.com>
2024-11-25 16:36:12 +0100

    Update script_cavalerie.race.js

--- js/script_cavalerie.race.js
+++ js/script_cavalerie.race.js
@@ -1,7 +1,7 @@
-// Autocompletion pour la race
+// autocompletion
 function autocompletRace(id) {
     var min_length = 1;
-    var keyword = $('#' + id + 'librace').val();
+    var keyword = $('#librace' + id).val();
 
     if (keyword.length >= min_length) {
         $.ajax({
@@ -9,23 +9,51 @@ function autocompletRace(id) {
             type: 'POST',
             data: {
                 keyword: keyword,
-                index: id
+                index: id // Envoie l'index à ajax_produit.php
             },
             success: function(data) {
-                $('#' + id + 'nom_list_race_id').show();
-                $('#' + id + 'nom_list_race_id').html(data);
+                $('#nom_list_race_id' + id).show();
+                $('#nom_list_race_id' + id).html(data);
             },
         });
     } else {
-        $('#' + id + 'nom_list_race_id').hide();
+        $('#nom_list_race_id' + id).hide();
+    }
+}
+
+function autocompletRaceajout() {
+    var min_length = 1;
+    var keyword = $('#librace').val();
+
+    if (keyword.length >= min_length) {
+        $.ajax({
+            url: '../ajax/ajax.cavalerie.raceajout.php',
+            type: 'POST',
+            data: {
+                keyword: keyword,
+            },
+            success: function(data) {
+                $('#nom_list_race_id').show();
+                $('#nom_list_race_id').html(data);
+            },
+        });
+    } else {
+        $('#nom_list_race_id').hide();
     }
 }
 
 // Lors du choix dans la liste
-function set_item(item, id, id_race, type) {
-    if (type === 'race') {
-        $('#' + id + 'librace').val(item);
-        $('#' + id + 'nom_list_race_id').hide();
-        $('#' + id + 'id_race').val(id_race);
-    }
+function set_item_race(item, id, id_race) {
+    // Mettre à jour le champ libgalop avec la valeur sélectionnée
+    $('#librace' + id).val(item);
+    // Cacher la liste de suggestions
+    $('#nom_list_race_id' + id).hide();
+    // Mettre à jour le champ caché num_sire avec l'ID de la catégorie sélectionnée
+    $('#id_race' + id).val(id_race);
+}
+
+function set_item_race_ajout(item, id_race) {
+    $('#librace').val(item);
+    $('#nom_list_race_id').hide();
+    $('#id_race').val(id_race);
 }
