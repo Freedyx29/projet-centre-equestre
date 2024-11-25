3b585b7077b8295df119c7c6674c140002e9202d
Freedyx29 <147407587+Freedyx29@users.noreply.github.com>
2024-11-25 16:44:32 +0100

    Update ajax.cavaliersajout.php

--- ajax/ajax.cavaliersajout.php
+++ ajax/ajax.cavaliersajout.php
@@ -14,7 +14,8 @@ if(isset($_POST['keyword'])) {
 
     foreach ($list as $res) {
         $Listegalopajout = str_replace($_POST['keyword'], '<b>'.$_POST['keyword'].'</b>', $res['libgalop']);
-        echo '<li onclick="set_item_cavalier_ajout(\''.str_replace("'", "\'", $res['libgalop']).'\',  '.$res['idgalop'].')">'.$Listegalopajout.'</li>';
+        // Utilise l'index `id` pour l'élément HTML, qui est passé depuis la fonction autocomplet() dans le JavaScript
+        echo '<li onclick="set_item_galop_ajout(\''.str_replace("'", "\'", $res['libgalop']).'\',  '.$res['idgalop'].')">'.$Listegalopajout.'</li>';
     }
 }
 ?>
