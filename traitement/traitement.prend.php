40dd22017b0a75a15c2893295824231868ac6cd6
Freedyx29 <147407587+Freedyx29@users.noreply.github.com>
2024-11-27 10:38:37 +0100

    Update traitement.prend.php

--- traitement/traitement.prend.php
+++ traitement/traitement.prend.php
@@ -8,7 +8,7 @@ if (isset($_POST['ajouter'])) {
     $refidcava = $_POST['idcava'];
     $refidpen = $_POST['idpen'];
     $success = $oprend->PrendAjt($refidcava, $refidpen) ? 1 : 0;
-    header("Location: vue.prend.php?success=$success");
+    header("Location: ../vue/vue.prend.php?success=1&message=Prêt ajouté avec succès");
 }
 
 if (isset($_POST['modifier'])) {
@@ -17,14 +17,14 @@ if (isset($_POST['modifier'])) {
     $refidcava = $_POST['idcava'];
     $refidpen = $_POST['idpen'];
     $success = $oprend->Modifier($id_cava_first, $id_pen_first, $refidcava, $refidpen) ? 1 : 0;
-    header("Location: vue.prend.php?success=$success");
+    header("Location: ../vue/vue.prend.php?success=1&message=Prêt modifié avec succès");
 }
 
 if (isset($_POST['supprimer'])) {
     $refidcava = $_POST['refidcava'];
     $refidpen = $_POST['refidpen'];
     $success = $oprend->Supprimer($refidcava, $refidpen) ? 1 : 0;
-    header("Location: vue.prend.php?success=$success");
+    header("Location: ../vue/vue.prend.php?success=1&message=Prêt supprimé avec succès");
 }
 
 ?>
