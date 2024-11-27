42488cab91344c9c182d81c1c80e550aa9770d80
Freedyx29 <147407587+Freedyx29@users.noreply.github.com>
2024-11-27 10:39:42 +0100

    Update script_prend.cava.js

--- js/script_prend.cava.js
+++ js/script_prend.cava.js
@@ -1,60 +1,59 @@
 // autocompletion
-function autocompletPrendCava(id) {
+function autocompletPrendPen(id) {
     var min_length = 1;
-    var keyword = $('#nomcava' + id).val();
+    var keyword = $('#libpen' + id).val();
 
     if (keyword.length >= min_length) {
         $.ajax({
-            url: '../ajax/ajax.prend.idcava.php', 
+            url: '../ajax/ajax.prend.idpen.php', 
             type: 'POST',
             data: { 
                 keyword: keyword,
                 index: id // Envoie l'index à ajax_produit.php
             },
             success: function(data) {
-                $('#nom_list_prend_cava_id' + id).show();
-                $('#nom_list_prend_cava_id' + id).html(data);
+                $('#nom_list_prend_pen_id' + id).show();
+                $('#nom_list_prend_pen_id' + id).html(data);
             },
         });
     } else {
-        $('#nom_list_prend_cava_id' + id).hide();
+        $('#nom_list_prend_pen_id' + id).hide();
     }
 }
 
-function autocompletPrendCavaajout() {
+function autocompletPrendPenajout() {
     var min_length = 1;
-    var keyword = $('#nomcava').val();
+    var keyword = $('#libpen').val();
 
     if (keyword && keyword.length >= min_length) {
         $.ajax({
-            url: '../ajax/ajax.prend.idcavaajout.php', 
+            url: '../ajax/ajax.prend.idpenajout.php', 
             type: 'POST',
             data: { 
                 keyword: keyword,
             },
             success: function(data) {
-                $('#nom_list_prend_cava_id').show();
-                $('#nom_list_prend_cava_id').html(data);
+                $('#nom_list_prend_pen_id').show();
+                $('#nom_list_prend_pen_id').html(data);
             },
         });
     } else {
-        $('#nom_list_prend_cava_id').hide();
+        $('#nom_list_prend_pen_id').hide();
     }
 }
 
-
 // Lors du choix dans la liste
-function set_item_cava(item, id, id_cava) {
+function set_item_pen(item, id, id_pen) {
     // Mettre à jour le champ nomcom avec la valeur sélectionnée
-    $('#nomcava' + id).val(item);
+    $('#libpen' + id).val(item);
     // Cacher la liste de suggestions
-    $('#nom_list_prend_cava_id' + id).hide();
+    $('#nom_list_prend_pen_id' + id).hide();
     // Mettre à jour le champ caché id_cat avec l'ID de la catégorie sélectionnée
-    $('#id_cava' + id).val(id_cava);
+    $('#id_pen' + id).val(id_pen);
 }
 
-function set_item_cava_ajout(item, id_cava) {
-    $('#nomcava').val(item);
-    $('#nom_list_prend_cava_id').hide();
-    $('#id_cava').val(id_cava);
+function set_item_pen_ajout(item, id_pen) {
+    $('#libpen').val(item);
+    $('#nom_list_prend_pen_id').hide();
+    $('#id_pen').val(id_pen);
 }
