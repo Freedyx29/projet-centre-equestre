236e01ebbcce9b0a3e3758a1368138d3df098de2
Freedyx29 <147407587+Freedyx29@users.noreply.github.com>
2024-11-28 10:16:51 +0100

    Add files via upload

---
+++ include/haut.inc.php
@@ -0,0 +1,19 @@
+<nav class="navbar">
+    <div class="container">
+        <a class="navbar-brand" href="#">
+            <img src="../photos/equi.png" alt="Logo Centre Équestre">
+        </a>
+        <div class="navbar-links">
+            <a href="../vue/vue.race.php">Races</a>
+            <a href="../vue/vue.robe.php">Robes</a>
+            <a href="../vue/vue.galop.php">Galops</a>
+            <a href="../vue/vue.cavaliers.php">Cavaliers</a>
+            <a href="../vue/vue.cavalerie.php">Cavalerie</a>
+            <a href="../vue/vue.cours.php">Cours</a>
+            <a href="../vue/vue.inscrit.php">Inscriptions</a>
+            <a href="../vue/vue.pension.php">Pensions</a>
+            <a href="../vue/vue.prend.php">Prêts</a>
+            <a href="../vue/vue.evenements.php">Événements</a>
+        </div>
+    </div>
+</nav>
