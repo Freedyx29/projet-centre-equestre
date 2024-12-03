80ccc8103b9751d43c373cd602fafe5b98bfcb36
rouan52 <ayarouan52@gmail.com>
2024-12-03 08:55:37 +0100

    Update vue.cours.php

--- vue/vue.cours.php
+++ vue/vue.cours.php
@@ -36,7 +36,7 @@ $listeCours = $cours->CoursAll();
                 Ajouter un cours
             </button>
             <!-- Bouton "Afficher le PDF" avec une couleur légèrement plus foncée -->
-            <a href="../classpdf/classpdfcours.php" class="btn" style="background-color: #B88C47; color: white; text-decoration: none; border-radius: 6px; padding: 10px 20px; font-size: 16px; font-family: Arial, sans-serif;">
+            <a href="../classpdf/classpdfcours.php" class="btn" style="background-color: #B88C47; color: white; text-decoration: none; border-radius: 6px; padding: 10px 20px; font-size: 16px; font-family: Arial, sans-serif "target="_blank";">
                 📋 Afficher le PDF
             </a>
         </div>
