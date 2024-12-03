b97bb349a4cdd50f02edf8a0ccf20ae856823712
rouan52 <ayarouan52@gmail.com>
2024-12-03 09:02:56 +0100

    Update vue.robe.php

--- vue/vue.robe.php
+++ vue/vue.robe.php
@@ -37,7 +37,7 @@ $listeRobes = $robe->RobeAll();
             Ajouter une robe
         </button>
         <!-- Bouton "Afficher le PDF" avec une couleur légèrement plus foncée -->
-        <a href="../classpdf/classpdfrobe.php" class="btn" style="background-color: #B88C47; color: white; text-decoration: none; border-radius: 6px; padding: 10px 20px; font-size: 16px; font-family: Arial, sans-serif;">
+        <a href="../classpdf/classpdfrobe.php" class="btn" style="background-color: #B88C47; color: white; text-decoration: none; border-radius: 6px; padding: 10px 20px; font-size: 16px; font-family: Arial, sans-serif "target="_blank";>
             📋 Afficher le PDF
         </a>
     </div>
