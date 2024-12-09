703f349434ddc1a34c63b04cbf24f87d48d6d67b
rouan52 <ayarouan52@gmail.com>
2024-12-09 18:57:46 +0100

    Update haut.inc.php

--- include/haut.inc.php
+++ include/haut.inc.php
@@ -1,22 +1,31 @@
-<nav class="navbar">
-    <div class="container">
-        <a class="navbar-brand" href="#">
-            <img src="../photos/equi.png" alt="Logo Centre Équestre">
-        </a>
-        <div class="navbar-links">
-            <a href="../vue/vue.race.php">Races</a>
-            <a href="../vue/vue.robe.php">Robes</a>
-            <a href="../vue/vue.galop.php">Galops</a>
-            <a href="../vue/vue.cavaliers.php">Cavaliers</a>
-            <a href="../vue/vue.cavalerie.php">Cavalerie</a>
-            <a href="../vue/vue.cours.php">Cours</a>
-            <a href="../vue/vue.inscrit.php">Inscriptions</a>
-            <a href="../vue/vue.pension.php">Pensions</a>
-            <a href="../vue/vue.evenements.php">Événements</a>
-            <a href="../utilisateurs/logout.php" class="btn-equestre">
-                <i class="fas fa-door-open"></i>
-                <span>Déconnexion</span>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Centre Équestre</title>
+    <link rel="stylesheet" href="../css/style_crud.css">
+</head>
+<body>
+    <nav class="navbar">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img src="../photos/equi.png" alt="Logo Centre Équestre">
             </a>
+            <div class="navbar-links">
+                <a href="../vue/vue.race.php">Races</a>
+                <a href="../vue/vue.robe.php">Robes</a>
+                <a href="../vue/vue.galop.php">Galops</a>
+                <a href="../vue/vue.cavaliers.php">Cavaliers</a>
+                <a href="../vue/vue.cavalerie.php">Cavalerie</a>
+                <a href="../vue/vue.cours.php">Cours</a>
+                <a href="../vue/vue.inscrit.php">Inscriptions</a>
+                <a href="../vue/vue.pension.php">Pensions</a>
+                <a href="../vue/vue.evenements.php">Événements</a>
+                <!-- Ajout du bouton de déconnexion -->
+                <a href="../utilisateurs/logout.php" class="btn btn-danger">Déconnexion</a>
+            </div>
         </div>
-    </div>
-</nav>
+    </nav>
+</body>
+</html>
