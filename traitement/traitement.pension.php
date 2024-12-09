db61352ed8a226f08eb4bca934f99e8291b5e5bd
rouan52 <ayarouan52@gmail.com>
2024-12-09 16:00:19 +0100

    Update traitement.pension.php

--- traitement/traitement.pension.php
+++ traitement/traitement.pension.php
@@ -10,28 +10,31 @@ if (isset($_POST['ajouter'])) {
     $dateF = $_POST['dateF'];
     $tarif = $_POST['tarif'];
     $numsire = $_POST['numsire'];
-    $success = $opension->PensionAjt($libpen, $dateD, $dateF, $tarif, $numsire) ? 1 : 0;
-    header('Location: ../vue/vue.pension.php?success=1&message=Pension ajoutée avec succès');
-    exit();
-}
+    $idcava = $_POST['idcava'];
 
-if (isset($_POST['modifier'])) {
-    $idpen = $_POST['idpen'];
-    $libpen = $_POST['libpen'];
-    $dateD = $_POST['dateD'];
-    $dateF = $_POST['dateF'];
-    $tarif = $_POST['tarif'];
-    $numsire = $_POST['numsire'];
-    $success = $opension->Modifier($idpen, $libpen, $dateD, $dateF, $tarif, $numsire) ? 1 : 0;
-    header('Location: ../vue/vue.pension.php?success=1&message=Pension modifiée avec succès');
-    exit();
-}
+    $idpen = $opension->PensionAjt($libpen, $dateD, $dateF, $tarif, $numsire);
 
-if (isset($_POST['supprimer'])) {
-    $idpen = $_POST['idpen'];
-    $success = $opension->Supprimer($idpen) ? 1 : 0;
-    header('Location: ../vue/vue.pension.php?success=1&message=Pension supprimée avec succès');
-    exit();
+    if ($idpen) {
+        $con = connexionPDO();
+        $data = [
+            ':refidcava' => $idcava,
+            ':refidpen' => $idpen
+        ];
+
+        $sql = "INSERT INTO prend (refidcava, refidpen) VALUES (:refidcava, :refidpen);";
+        $stmn = $con->prepare($sql);
+
+        if ($stmn->execute($data)) {
+            header('Location: ../vue/vue.pension.php?success=1&message=Pension ajoutée avec succès');
+            exit();
+        } else {
+            header('Location: ../vue/vue.pension.php?success=0&message=Erreur lors de l\'ajout de la pension');
+            exit();
+        }
+    } else {
+        header('Location: ../vue/vue.pension.php?success=0&message=Erreur lors de l\'ajout de la pension');
+        exit();
+    }
 }
 
 ?>
