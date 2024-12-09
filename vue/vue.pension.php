5eab39c4eb152586012ec7f38d7ae79d5cfa727d
rouan52 <ayarouan52@gmail.com>
2024-12-09 15:59:40 +0100

    Update vue.pension.php

--- vue/vue.pension.php
+++ vue/vue.pension.php
@@ -86,192 +86,171 @@ $listePensions = $pension->PensionALL();
                 <?php endforeach; ?>
             </tbody>
         </table>
-
-        <!-- Modal Ajout -->
-        <div class="modal fade" id="ajoutModal">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h4 class="modal-title">Ajouter une pension</h4>
-                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+<!-- Modal Ajout -->
+<div class="modal fade" id="ajoutModal">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title">Ajouter une pension</h4>
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+            </div>
+            <form action="../traitement/traitement.pension.php" method="post">
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label>Libellé</label>
+                        <input type="text" name="libpen" class="form-control" required>
                     </div>
-                    <form action="../traitement/traitement.pension.php" method="post">
+                    <div class="form-group">
+                        <label>Date début</label>
+                        <input type="date" name="dateD" class="form-control" required>
+                    </div>
+                    <div class="form-group">
+                        <label>Date fin</label>
+                        <input type="date" name="dateF" class="form-control" required>
+                    </div>
+                    <div class="form-group">
+                        <label>Tarif</label>
+                        <input type="number" name="tarif" class="form-control" step="1" required>
+                    </div>
+                    <div class="form-group">
+                        <label>Cheval</label>
+                        <input type="text" id="nomche" class="form-control" onkeyup="autocompletPensionajout()" required>
+                        <input type="hidden" id="num_sire" name="numsire">
+                        <ul id="nom_list_pension_id"></ul>
+                    </div>
+                    <div class="form-group">
+                        <label>Nom Cavalier</label>
+                        <input type="text" id="nomcava" class="form-control" onkeyup="autocompletCavalierajout()" required>
+                        <input type="hidden" id="idcava" name="idcava">
+                        <ul id="nom_list_cavalier_id"></ul>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="submit" name="ajouter" class="btn btn-primary">Ajouter</button>
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Fermer</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
 
-                        <div class="modal-body">
-
-                            <div class="form-group">
-                                <label>Libellé</label>
-                                <input type="text" name="libpen" class="form-control" required>
-                            </div>
-
-                            <div class="form-group">
-                                <label>Date début</label>
-                                <input type="date" name="dateD" class="form-control" required>
-                            </div>
-
-                            <div class="form-group">
-                                <label>Date fin</label>
-                                <input type="date" name="dateF" class="form-control" required>
-                            </div>
-
-                            <div class="form-group">
-                                <label>Tarif</label>
-                                <input type="number" name="tarif" class="form-control" step="1" required>
-                            </div>
-
-                            <div class="form-group">
-                                <label>Cheval</label>
-                                <input type="text" id="nomche" class="form-control" onkeyup="autocompletPensionajout()" required>
-                                <input type="hidden" id="num_sire" name="numsire">
-                                <ul id="nom_list_pension_id"></ul>
-                            </div>
-
+<!-- Modals Modification et Suppression -->
+<?php foreach($listePensions as $p): ?>
+    <!-- Modal Modification -->
+    <div class="modal fade" id="modifModal<?php echo $p['idpen']; ?>">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title">Modifier la pension</h4>
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                </div>
+                <form action="../traitement/traitement.pension.php" method="post">
+                    <div class="modal-body">
+                        <input type="hidden" name="idpen" value="<?php echo $p['idpen']; ?>">
+                        <div class="form-group">
+                            <label>Libellé</label>
+                            <input type="text" name="libpen" class="form-control" value="<?php echo $p['libpen']; ?>" required>
                         </div>
-
-                        <div class="modal-footer">
-                            <button type="submit" name="ajouter" class="btn btn-primary">Ajouter</button>
-                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Fermer</button>
+                        <div class="form-group">
+                            <label>Date début</label>
+                            <input type="date" name="dateD" class="form-control" value="<?php echo $p['dateD']; ?>" required>
                         </div>
+                        <div class="form-group">
+                            <label>Date fin</label>
+                            <input type="date" name="dateF" class="form-control" value="<?php echo $p['dateF']; ?>" required>
+                        </div>
+                        <div class="form-group">
+                            <label>Tarif</label>
+                            <input type="number" name="tarif" class="form-control" step="1" value="<?php echo $p['tarif']; ?>" required>
+                        </div>
+                        <div class="form-group">
+                            <label>Cheval</label>
+                            <input type="text" id="nomche<?php echo $p['idpen']; ?>" class="form-control"
+                                   value="<?php echo $pension->PensionNumsire($p['numsire']); ?>"
+                                   onkeyup="autocompletPension(<?php echo $p['idpen']; ?>)" required>
+                            <input type="hidden" id="num_sire<?php echo $p['idpen']; ?>" name="numsire" value="<?php echo $p['numsire']; ?>">
+                            <ul id="nom_list_pension_id<?php echo $p['idpen']; ?>"></ul>
+                        </div>
+                        <div class="form-group">
+                            <label>Nom Cavalier</label>
+                            <input type="text" id="nomcava<?php echo $p['idpen']; ?>" class="form-control" onkeyup="autocompletCavaliermodif(<?php echo $p['idpen']; ?>)" required>
+                            <input type="hidden" id="idcava<?php echo $p['idpen']; ?>" name="idcava" value="<?php echo $p['idcava']; ?>">
+                            <ul id="nom_list_cavalier_id<?php echo $p['idpen']; ?>"></ul>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="submit" name="modifier" class="btn btn-warning">Modifier</button>
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Fermer</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 
-                    </form>
+    <!-- Modal Suppression -->
+    <div class="modal fade" id="suppModal<?php echo $p['idpen']; ?>">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title">Confirmer la suppression</h4>
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                </div>
+                <div class="modal-body">
+                    Êtes-vous sûr de vouloir supprimer cette pension ?
+                </div>
+                <form action="../traitement/traitement.pension.php" method="post">
+                    <input type="hidden" name="idpen" value="<?php echo $p['idpen']; ?>">
+                    <div class="modal-footer">
+                        <button type="submit" name="supprimer" class="btn btn-danger">Supprimer</button>
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Annuler</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <!-- Modal Détail -->
+    <div class="modal fade" id="detailModal<?php echo $p['idpen']; ?>">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title">Détails de la pension</h4>
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                </div>
+                <div class="modal-body">
+                    <div class="detail-group">
+                        <label>ID Pension :</label>
+                        <p><?php echo $p['idpen']; ?></p>
+                    </div>
+                    <div class="detail-group">
+                        <label>Libellé :</label>
+                        <p><?php echo $p['libpen']; ?></p>
+                    </div>
+                    <div class="detail-group">
+                        <label>Date de début :</label>
+                        <p><?php echo date('d/m/Y', strtotime($p['dateD'])); ?></p>
+                    </div>
+                    <div class="detail-group">
+                        <label>Date de fin :</label>
+                        <p><?php echo date('d/m/Y', strtotime($p['dateF'])); ?></p>
+                    </div>
+                    <div class="detail-group">
+                        <label>Tarif :</label>
+                        <p><?php echo $p['tarif']; ?> €</p>
+                    </div>
+                    <div class="detail-group">
+                        <label>Cheval :</label>
+                        <p><?php echo $pension->PensionNumsire($p['numsire']); ?></p>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Fermer</button>
                 </div>
             </div>
         </div>
-
-        <!-- Modals Modification et Suppression et  -->
-        <?php foreach($listePensions as $p): ?>
-            <!-- Modal Modification -->
-            <div class="modal fade" id="modifModal<?php echo $p['idpen']; ?>">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h4 class="modal-title">Modifier la pension</h4>
-                            <button type="button" class="close" data-dismiss="modal">&times;</button>
-                        </div>
-                        <form action="../traitement/traitement.pension.php" method="post">
-                            <div class="modal-body">
-
-                                <input type="hidden" name="idpen" value="<?php echo $p['idpen']; ?>">
-
-                                <div class="form-group">
-                                    <label>Libellé</label>
-                                    <input type="text" name="libpen" class="form-control" value="<?php echo $p['libpen']; ?>" required>
-                                </div>
-
-                                <div class="form-group">
-                                    <label>Date début</label>
-                                    <input type="date" name="dateD" class="form-control" value="<?php echo $p['dateD']; ?>" required>
-                                </div>
-
-                                <div class="form-group">
-                                    <label>Date fin</label>
-                                    <input type="date" name="dateF" class="form-control" value="<?php echo $p['dateF']; ?>" required>
-                                </div>
-
-                                <div class="form-group">
-                                    <label>Tarif</label>
-                                    <input type="number" name="tarif" class="form-control" step="1" value="<?php echo $p['tarif']; ?>" required>
-                                </div>
-
-                                <div class="form-group">
-                                    <label>Cheval</label>
-                                    <input type="text" id="nomche<?php echo $p['idpen']; ?>" class="form-control" 
-                                           value="<?php echo $pension->PensionNumsire($p['numsire']); ?>" 
-                                           onkeyup="autocompletPension(<?php echo $p['idpen']; ?>)" required>
-
-                                    <input type="hidden" id="num_sire<?php echo $p['idpen']; ?>" name="numsire" value="<?php echo $p['numsire']; ?>">
-
-                                    <ul id="nom_list_pension_id<?php echo $p['idpen']; ?>"></ul>
-                                </div>
-
-                            </div>
-
-                            <div class="modal-footer">
-                                <button type="submit" name="modifier" class="btn btn-warning">Modifier</button>
-                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Fermer</button>
-                            </div>
-
-                        </form>
-                    </div>
-                </div>
-            </div>
-
-            <!-- Modal Suppression -->
-            <div class="modal fade" id="suppModal<?php echo $p['idpen']; ?>">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h4 class="modal-title">Confirmer la suppression</h4>
-
-                            <button type="button" class="close" data-dismiss="modal">&times;</button>
-                        </div>
-
-                        <div class="modal-body">
-                            Êtes-vous sûr de vouloir supprimer cette pension ?
-                        </div>
-
-                        <form action="../traitement/traitement.pension.php" method="post">
-                            <input type="hidden" name="idpen" value="<?php echo $p['idpen']; ?>">
-
-                            <div class="modal-footer">
-                                <button type="submit" name="supprimer" class="btn btn-danger">Supprimer</button>
-                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Annuler</button>
-                            </div>
-
-                        </form>
-                    </div>
-                </div>
-            </div>
-
-            <!-- Modal Détail -->
-            <div class="modal fade" id="detailModal<?php echo $p['idpen']; ?>">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h4 class="modal-title">Détails de la pension</h4>
-                            <button type="button" class="close" data-dismiss="modal">&times;</button>
-                        </div>
-
-                        <div class="modal-body">
-                            <div class="detail-group">
-                                <label>ID Pension :</label>
-                                <p><?php echo $p['idpen']; ?></p>
-                            </div>
-
-                            <div class="detail-group">
-                                <label>Libellé :</label>
-                                <p><?php echo $p['libpen']; ?></p>
-                            </div>
-
-                            <div class="detail-group">
-                                <label>Date de début :</label>
-                                <p><?php echo date('d/m/Y', strtotime($p['dateD'])); ?></p>
-                            </div>
-
-                            <div class="detail-group">
-                                <label>Date de fin :</label>
-                                <p><?php echo date('d/m/Y', strtotime($p['dateF'])); ?></p>
-                            </div>
-
-                            <div class="detail-group">
-                                <label>Tarif :</label>
-                                <p><?php echo $p['tarif']; ?> €</p>
-                            </div>
-
-                            <div class="detail-group">
-                                <label>Cheval :</label>
-                                <p><?php echo $pension->PensionNumsire($p['numsire']); ?></p>
-                            </div>
-
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Fermer</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        <?php endforeach; ?>
     </div>
+<?php endforeach; ?>
+
 
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
