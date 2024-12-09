ea365c530cbf7d102cc9c708d3a4d75bcdb1ad73
Freedyx29 <147407587+Freedyx29@users.noreply.github.com>
2024-12-09 17:41:03 +0100

    Update traitement.pension.php

--- traitement/traitement.pension.php
+++ traitement/traitement.pension.php
@@ -37,4 +37,23 @@ if (isset($_POST['ajouter'])) {
     }
 }
 
+if (isset($_POST['modifier'])) {
+    $idpen = $_POST['idpen'];
+    $libpen = $_POST['libpen'];
+    $dateD = $_POST['dateD'];
+    $dateF = $_POST['dateF'];
+    $tarif = $_POST['tarif'];
+    $numsire = $_POST['numsire'];
+    $success = $opension->Modifier($idpen, $libpen, $dateD, $dateF, $tarif, $numsire) ? 1 : 0;
+    header('Location: ../vue/vue.pension.php?success=1&message=Pension modifiée avec succès');
+    exit();
+}
+
+if (isset($_POST['supprimer'])) {
+    $idpen = $_POST['idpen'];
+    $success = $opension->Supprimer($idpen) ? 1 : 0;
+    header('Location: ../vue/vue.pension.php?success=1&message=Pension supprimée avec succès');
+    exit();
+}
+
 ?>
