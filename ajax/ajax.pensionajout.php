6b495102a23973924954172ff57a1a23ea92df8e
rouan52 <ayarouan52@gmail.com>
2024-12-09 16:01:35 +0100

    Update ajax.pensionajout.php

--- ajax/ajax.pensionajout.php
+++ ajax/ajax.pensionajout.php
@@ -13,9 +13,8 @@ if(isset($_POST['keyword'])) {
     $list = $req->fetchAll();
 
     foreach ($list as $res) {
-        $Listepensionajout = str_replace($_POST['keyword'], '<b>'.$_POST['keyword'].'</b>', $res['nomche']);
-        // Utilise l'index `id` pour l'élément HTML, qui est passé depuis la fonction autocomplet() dans le JavaScript
-        echo '<li onclick="set_item_pension_ajout(\''.str_replace("'", "\'", $res['nomche']).'\',  '.$res['numsire'].')">'.$Listepensionajout.'</li>';
+        $Listechevaux = str_replace($_POST['keyword'], '<b>'.$_POST['keyword'].'</b>', $res['nomche']);
+        echo '<li onclick="set_item_pension_ajout(\''.str_replace("'", "\'", $res['nomche']).'\', '.$res['numsire'].')">'.$Listechevaux.'</li>';
     }
 }
 ?>
