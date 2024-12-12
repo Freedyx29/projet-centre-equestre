0afa9d4f7b957cc0fbfa4d1de644ba08fe0f7948
rouan52 <ayarouan52@gmail.com>
2024-12-12 11:44:30 +0100

    Update ajax.pensionajout (1).php

--- ajax/ajax.pensionajout (1).php
+++ ajax/ajax.pensionajout (1).php
@@ -1,17 +1,24 @@
 <?php
 include '../include/bdd.inc.php';
+
 if(isset($_POST['keyword'])) {
-	$con = connexionPDO();
-$keyword = '%'.$_POST['keyword'].'%';
-$sql = "SELECT * FROM pension WHERE libpen LIKE (:var) ORDER BY idpen ASC LIMIT 0, 100";
-$req = $con->prepare($sql);
-$req->bindParam(':var', $keyword, PDO::PARAM_STR);
-$req->execute();
-$list = $req->fetchAll();
-foreach ($list as $res) {
-    $Listepensionajout = str_replace($_POST['keyword'], '<b>'.$_POST['keyword'].'</b>', $res['libpen']);
-    // Utilise l'index `id` pour l'élément HTML, qui est passé depuis la fonction autocomplet() dans le JavaScript
-    echo '<li onclick="set_item_pen_ajout(\''.str_replace("'", "\'", $res['libpen']).'\',  '.$res['idpen'].')">'.$Listepensionajout.'</li>';
+    $con = connexionPDO();
+    $keyword = '%'.$_POST['keyword'].'%';
+
+    $sql = "SELECT * FROM cavalerie WHERE nomche LIKE (:var) ORDER BY numsire ASC LIMIT 0, 100";
+    $req = $con->prepare($sql);
+    $req->bindParam(':var', $keyword, PDO::PARAM_STR);
+    $req->execute();
+
+    $list = $req->fetchAll();
+
+    foreach ($list as $res) {
+        $Listechevaux = str_replace($_POST['keyword'], '<b>'.$_POST['keyword'].'</b>', $res['nomche']);
+
+        // Vérifiez si la clé 'index' existe dans le tableau $_POST
+        $index = isset($_POST['index']) ? $_POST['index'] : 0;
+
+        echo '<li onclick="set_item_pension_ajout(\''.str_replace("'", "\'", $res['nomche']).'\', '.$index.', '.$res['numsire'].')">'.$Listechevaux.'</li>';
     }
 }
 ?>
