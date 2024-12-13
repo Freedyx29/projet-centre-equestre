0f3d2bcb3c77e6e67a278fb4f8388af6043efdd9
Freedyx29 <147407587+Freedyx29@users.noreply.github.com>
2024-12-13 20:44:56 +0100

    Update vue.inscrit.php

--- vue/vue.inscrit.php
+++ vue/vue.inscrit.php
@@ -1,4 +1,12 @@
 <?php
+session_start();
+
+// Vérifier si l'utilisateur est connecté
+if (!isset($_SESSION['iduti'])) {
+    $current_page = urlencode($_SERVER['PHP_SELF']);
+    header("Location: ../vue/vue.index.php?redirect_to=" . $current_page);
+    exit();
+}
 
 include_once '../include/haut.inc.php';
 include_once '../class/class.inscrit.php';
@@ -39,7 +47,7 @@ $listeInscrits = $inscrit->InscritALL();
                     Ajouter une inscription
                 </button>
                 <!-- Bouton "Afficher le PDF" avec une couleur légèrement plus foncée -->
-                <a href="../classpdf/classpdfinscrit.php" class="btn" style="background-color: #B88C47; color: white; text-decoration: none; border-radius: 6px; padding: 10px 20px; font-size: 16px; font-family: Arial, sans-serif;">
+                <a href="../classpdf/classpdfinscrit.php" class="btn" style="background-color: #B88C47; color: white; text-decoration: none; border-radius: 6px; padding: 10px 20px; font-size: 16px; font-family: Arial, sans-serif;" target="_blank">
                     📋 Afficher le PDF
                 </a>
             </div>
