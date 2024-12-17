53c6f48ce9873940fd8ab02b40b471513fe5135c
melikekiris <melikekiris24@gmail.com>
2024-12-17 11:49:46 +0100

    Update haut.inc.php

--- include/haut.inc.php
+++ include/haut.inc.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Centre Équestre</title>
     <link rel="stylesheet" href="../css/style_crud.css">
+    <!-- Ajouter Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
 <body>
     <nav class="navbar">
@@ -23,8 +25,10 @@
                 <a href="../vue/vue.pension.php">Pensions</a>
                 <a href="../vue/vue.evenements.php">Événements</a>
                 <a href="../vue/vue.calendrier.php">Calendrier</a>
-                <!-- Ajout du bouton de déconnexion -->
-                <a href="../utilisateurs/logout.php" class="btn btn-danger">Déconnexion</a>
+                <!-- Ajout du bouton de déconnexion avec icône -->
+                <a href="../utilisateurs/logout.php" class="btn btn-danger">
+                    <i class="fas fa-sign-out-alt"></i> Déconnexion
+                </a>
             </div>
         </div>
     </nav>
