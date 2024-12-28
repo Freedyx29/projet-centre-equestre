e1fa18b7fff2d4729f1fbdd402417bb6d3d64bc8
melikekiris <melikekiris24@gmail.com>
2024-12-28 17:12:26 +0100

    Update contact.php

--- Equisite/contact.php
+++ Equisite/contact.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8">
-    <title>Klinik - Clinic Website Template</title>
+    <title>Equihorizon - Centre Équestre</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="" name="keywords">
     <meta content="" name="description">
@@ -49,7 +49,7 @@
             </button>
 
             <!-- Centered logo -->
-            <a href="index.html" class="navbar-brand d-flex justify-content-center position-absolute start-50 translate-middle-x" style="top: -10px;"> <!-- Added top property -->
+            <a href="index.php" class="navbar-brand d-flex justify-content-center position-absolute start-50 translate-middle-x" style="top: -10px;"> <!-- Added top property -->
                 <img src="../photos/equip.png" alt="Logo" class="m-0" style="height: 100px;">
             </a>
 
@@ -162,13 +162,16 @@
         }
     </style>
 
-    <!-- Page Header Start -->
-    <div class="container-fluid page-header py-5 mb-5 wow fadeIn" data-wow-delay="0.1s">
-        <div class="container py-5">
-            <h1 class="display-3 text-white mb-3 animated slideInDown">Contactez-nous</h1>
-        </div>
+<!-- Page Header Start -->
+<div class="container-fluid page-header py-5 mb-5 wow fadeIn" data-wow-delay="0.1s" style="background-image: url('../photos/customer-service-business-contact-concept-wooden-cube-block-which-print-screen-letter-telephone-email-address-message.jpg'); background-size: cover; background-position: center; background-repeat: no-repeat; position: relative;">
+    <div style="position: absolute; top: 0; left: 0; right: 0; bottom: 0; background: rgba(0, 0, 0, 0.5); z-index: 1;"></div>
+    <div class="container py-5" style="position: relative; z-index: 2;">
+        <h1 class="display-3 text-white mb-3 animated slideInDown">Contactez-nous</h1>
     </div>
-    <!-- Page Header End -->
+</div>
+<!-- Page Header End -->
+
+
 
     <!-- Contact Start -->
     <div class="container-xxl py-5">
@@ -203,7 +206,7 @@
                         </div>
                         <div class="ms-4">
                             <p class="mb-2">Écrivez-nous</p>
-                            <h5 class="mb-0">info@example.com</h5>
+                            <h5 class="mb-0">equi@horizon.fr</h5>
                         </div>
                     </div>
                 </div>
@@ -211,7 +214,6 @@
                     <div class="bg-light rounded p-5">
                         <p class="d-inline-block border rounded-pill py-1 px-4">Contactez-nous</p>
                         <h1 class="mb-4">Avez-vous une question ? Contactez-nous !</h1>
-                        <p class="mb-4">Le formulaire de contact est actuellement inactif. Obtenez un formulaire de contact fonctionnel et opérationnel avec Ajax & PHP en quelques minutes. Il vous suffit de copier et coller les fichiers, d'ajouter un peu de code et c'est fait. <a href="https://htmlcodex.com/contact-form">Télécharger maintenant</a>.</p>
                         <form>
                             <div class="row g-3">
                                 <div class="col-md-6">
@@ -246,69 +248,65 @@
                     </div>
                 </div>
                 <div class="col-lg-6 wow fadeIn" data-wow-delay="0.5s">
-                    <div class="h-100" style="min-height: 400px;">
-                        <iframe class="rounded w-100 h-100"
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3001156.4288297426!2d-78.01371936852176!3d42.72876761954724!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4ccc4bf0f123a5a9%3A0xddcfc6c1de189567!2sNew%20York%2C%20USA!5e0!3m2!1sen!2sbd!4v1603794290143!5m2!1sen!2sbd"
-                        frameborder="0" allowfullscreen="" aria-hidden="false"
-                        tabindex="0"></iframe>
-                    </div>
+                <div class="h-100" style="min-height: 400px;">
+                    <iframe class="rounded w-100 h-100"
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2856.527784304884!2d-0.579659384443559!3d44.83954297909838!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd5527e3e8f9b5cb%3A0x40d3983e19ab730!2s35%20Rue%20de%20la%20Benatte%2C%2033000%20Bordeaux%2C%20France!5e0!3m2!1sen!2sfr!4v1698649200000!5m2!1sen!2sfr"
+                            frameborder="0" allowfullscreen="" aria-hidden="false"
+                            tabindex="0"></iframe>
+                </div>
+
                 </div>
             </div>
         </div>
     </div>
     <!-- Contact End -->
 
-    <!-- Footer Start -->
-    <div class="container-fluid bg-dark text-light footer mt-5 pt-5 wow fadeIn" data-wow-delay="0.1s">
-        <div class="container py-5">
-            <div class="row g-5">
-                <div class="col-lg-3 col-md-6">
-                    <h5 class="text-light mb-4">Adresse</h5>
-                    <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>123 Rue, Paris, France</p>
-                    <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>+33 1 23 45 67 890</p>
-                    <p class="mb-2"><i class="fa fa-envelope me-3"></i>info@example.com</p>
-                    <div class="d-flex pt-2">
-                        <a class="btn btn-outline-light btn-social rounded-circle" href=""><i class="fab fa-facebook-f"></i></a>
-                        <a class="btn btn-outline-light btn-social rounded-circle" href=""><i class="fab fa-youtube"></i></a>
-                        <a class="btn btn-outline-light btn-social rounded-circle" href=""><i class="fab fa-linkedin-in"></i></a>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                    <h5 class="text-light mb-4">Services</h5>
-                    <a class="btn btn-link" href="">Équitation</a>
-                    <a class="btn btn-link" href="">Dressage</a>
-                    <a class="btn btn-link" href="">Saut d'obstacles</a>
-                    <a class="btn btn-link" href="">Cours pour enfants</a>
-                    <a class="btn btn-link" href="">Cours pour adultes</a>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                    <h5 class="text-light mb-4">Liens Rapides</h5>
-                    <a class="btn btn-link" href="">À propos de nous</a>
-                    <a class="btn btn-link" href="">Contactez-nous</a>
-                    <a class="btn btn-link" href="">Nos Services</a>
-                    <a class="btn btn-link" href="">Conditions Générales</a>
-                    <a class="btn btn-link" href="">Support</a>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                    <h5 class="text-light mb-4">Newsletter</h5>
-                    <div class="position-relative mx-auto" style="max-width: 400px;">
-                        <input class="form-control border-0 w-100 py-3 ps-4 pe-5" type="text" placeholder="Votre email">
-                        <button type="button" class="btn btn-primary py-2 position-absolute top-0 end-0 mt-2 me-2">S'inscrire</button>
-                    </div>
+<!-- Footer Start -->
+<div class="container-fluid bg-dark text-light footer mt-5 pt-5 wow fadeIn" data-wow-delay="0.1s">
+    <div class="container py-5">
+        <div class="row g-5">
+            <div class="col-lg-3 col-md-6">
+                <h5 class="text-light mb-4">Adresse</h5>
+                <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>123 Rue, Paris, France</p>
+                <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>+33 1 23 45 67 890</p>
+                <p class="mb-2"><i class="fa fa-envelope me-3"></i>equi@herizon.fr</p>
+                <div class="d-flex pt-2">
+                    <a class="btn btn-outline-light btn-social rounded-circle" href=""><i class="fab fa-facebook-f"></i></a>
+                    <a class="btn btn-outline-light btn-social rounded-circle" href=""><i class="fab fa-youtube"></i></a>
+                    <a class="btn btn-outline-light btn-social rounded-circle" href=""><i class="fab fa-linkedin-in"></i></a>
                 </div>
             </div>
+            <div class="col-lg-3 col-md-6">
+                <h5 class="text-light mb-4">Services</h5>
+                <p><i class="fas fa-horse me-3"></i>Équitation</p>
+                <p><i class="fas fa-horse-head me-3"></i>Dressage</p>
+                <p><i class="fas fa-horse me-3"></i>Saut d'obstacles</p>
+                <p><i class="fas fa-child me-3"></i>Cours pour enfants</p>
+                <p><i class="fas fa-user me-3"></i>Cours pour adultes</p>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <h5 class="text-light mb-4">Newsletter</h5>
+                <div class="position-relative mx-auto" style="max-width: 400px;">
+                    <input class="form-control border-0 w-100 py-3 ps-4 pe-5" type="text" placeholder="Votre email">
+                    <button type="button" class="btn btn-primary py-2 position-absolute top-0 end-0 mt-2 me-2">S'inscrire</button>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 text-center">
+                <img src="../photos/equi.png" alt="Logo" class="img-fluid" style="max-width: 150px;">
+            </div>
         </div>
-        <div class="container">
-            <div class="copyright text-center">
-                <div class="row">
-                    <div class="col-12">
-                        &copy; <a class="border-bottom" href="#">Equihorizon</a>, Tous Droits Réservés.
-                    </div>
+    </div>
+    <div class="container">
+        <div class="copyright text-center">
+            <div class="row">
+                <div class="col-12">
+                    &copy; Equihorizon, Tous Droits Réservés.
                 </div>
             </div>
         </div>
     </div>
-    <!-- Footer End -->
+</div>
+<!-- Footer End -->
 
     <!-- Back to Top -->
     <a href="#" class="btn btn-lg btn-primary btn-lg-square rounded-circle back-to-top"><i class="bi bi-arrow-up"></i></a>
