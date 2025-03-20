c5092eb50bedaa654e88ec8dbcb4b1047f1f3939
Freedyx29 <147407587+Freedyx29@users.noreply.github.com>
2025-03-20 11:14:22 +0100

    Update traitement.cavaliers.php

--- traitement/traitement.cavaliers.php
+++ traitement/traitement.cavaliers.php
@@ -1,5 +1,5 @@
 <?php
-session_start();
+
 include '../class/class.cavaliers.php';
 
 $ocavaliers = new Cavaliers();
@@ -8,6 +8,7 @@ if (isset($_POST['ajouter'])) {
     $nomcava = $_POST['nomcava'];
     $prenomcava = $_POST['prenomcava'];
     $datenacava = $_POST['datenacava'];
+    $emailcava = $_POST['emailcava'];
     $numlic = $_POST['numlic'];
     $nomresp = $_POST['nomresp'];
     $prenomresp = $_POST['prenomresp'];
@@ -19,14 +20,8 @@ if (isset($_POST['ajouter'])) {
     $password = $_POST['password'];
     $assurance = $_POST['assurance'];
     $idgalop = $_POST['idgalop'];
-
-    // Stocker le mot de passe en clair dans la session
-    $_SESSION['clear_password'][$emailresp] = $password;
-
-    // Message de débogage
-    error_log("Mot de passe en clair stocké pour l'email: $emailresp");
-
-    $success = $ocavaliers->CavaliersAjt($nomcava, $prenomcava, $datenacava, $numlic, $nomresp, $prenomresp, $rueresp, $vilresp, $cpresp, $telresp, $emailresp, $password, $assurance, $idgalop) ? 1 : 0;
+    $success = $ocavaliers->CavaliersAjt($nomcava, $prenomcava, $datenacava, $emailcava, $numlic,$nomresp, $prenomresp, $rueresp, $vilresp, $cpresp, $telresp,
+                                        $emailresp, $password, $assurance, $idgalop ) ? 1 : 0;
     header("Location: ../vue/vue.cavaliers.php?success=1&message=Cavalier ajouté avec succès");
 }
 
@@ -35,6 +30,7 @@ if (isset($_POST['modifier'])) {
     $nomcava = $_POST['nomcava'];
     $prenomcava = $_POST['prenomcava'];
     $datenacava = $_POST['datenacava'];
+    $emailcava = $_POST['emailcava'];
     $numlic = $_POST['numlic'];
     $nomresp = $_POST['nomresp'];
     $prenomresp = $_POST['prenomresp'];
@@ -46,14 +42,9 @@ if (isset($_POST['modifier'])) {
     $password = $_POST['password'];
     $assurance = $_POST['assurance'];
     $idgalop = $_POST['idgalop'];
-
-    // Stocker le mot de passe en clair dans la session
-    $_SESSION['clear_password'][$emailresp] = $password;
-
-    // Message de débogage
-    error_log("Mot de passe en clair stocké pour l'email: $emailresp");
-
-    $success = $ocavaliers->Modifier($idcava, $nomcava, $prenomcava, $datenacava, $numlic, $nomresp, $prenomresp, $rueresp, $vilresp, $cpresp, $telresp, $emailresp, $password, $assurance, $idgalop) ? 1 : 0;
+    $success = $ocavaliers->Modifier($idcava, $nomcava, $prenomcava, $datenacava, $emailcava, $numlic,
+    $nomresp, $prenomresp, $rueresp, $vilresp, $cpresp, $telresp,
+    $emailresp, $password, $assurance, $idgalop) ? 1 : 0;
     header("Location: ../vue/vue.cavaliers.php?success=1&message=Cavalier modifié avec succès");
 }
 
@@ -62,4 +53,5 @@ if (isset($_POST['supprimer'])) {
     $success = $ocavaliers->Supprimer($idcava) ? 1 : 0;
     header("Location: ../vue/vue.cavaliers.php?success=1&message=Cavalier supprimé avec succès");
 }
+
 ?>
