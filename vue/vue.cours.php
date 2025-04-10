cf220ff9af943c1e8af478cbc5d58b2089370129
Freedyx29 <147407587+Freedyx29@users.noreply.github.com>
2025-04-10 13:24:43 +0200

    Update vue.cours.php

--- vue/vue.cours.php
+++ vue/vue.cours.php
@@ -2,7 +2,7 @@
 session_start();
 
 // Vérifier si l'utilisateur est connecté
-if (!isset($_SESSION['iduti'])) {
+if (!isset($_SESSION['idadmin'])) {
     $current_page = urlencode($_SERVER['PHP_SELF']);
     header("Location: ../vue/vue.index.php?redirect_to=" . $current_page);
     exit();
