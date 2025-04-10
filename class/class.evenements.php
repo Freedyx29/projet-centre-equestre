168064a210dcde76ac2f2b8c37a06d4a6227d9ef
Freedyx29 <147407587+Freedyx29@users.noreply.github.com>
2025-04-10 15:32:41 +0200

    Update class.evenements.php

--- class/class.evenements.php
+++ class/class.evenements.php
@@ -5,11 +5,13 @@ class Evenements {
     private $ideve;
     private $titre;
     private $commentaire;
+    private $photo;
 
-    public function __construct($ide = null, $tt = null, $cmt = null) {
+    public function __construct($ide = null, $tt = null, $cmt = null, $ph = null) {
         $this->ideve = $ide;
         $this->titre = $tt;
         $this->commentaire = $cmt;
+        $this->photo = $ph;
     }
 
     public function getEvenements() {
@@ -30,6 +32,10 @@ class Evenements {
         return $this->commentaire;
     }
 
+    public function getphoto() {
+        return $this->photo;
+    }
+
     public function settitre($tt) {
         $this->titre = $tt;
     }
@@ -37,6 +43,11 @@ class Evenements {
     public function setcommentaire($cmt) {
         $this->commentaire = $cmt;
     }
+
+    public function setphoto($ph) {
+        $this->photo = $ph;
+    }
+
 
     public function EvenementsAll(){
         $con = connexionPDO();
@@ -46,6 +57,18 @@ class Evenements {
         $resultat = $executesql->fetchAll();
         return $resultat;
     }
+
+
+    public function getSinglePhotoByIEve($ideve) {
+        $con = connexionPDO();
+        $sql = "SELECT lienphoto FROM photos WHERE ideve = :ideve LIMIT 1";
+        $data = [':ideve' => $ideve];
+        $executesql = $con->prepare($sql);
+        $executesql->execute($data);
+        $resultat = $executesql->fetch();
+        return $resultat ? $resultat['lienphoto'] : null;
+    }
+
 
     public function Modifier($id, $tt, $cmt){
         $con = connexionPDO();
@@ -118,7 +141,31 @@ class Evenements {
         }
     }
 
-    public function getPhotosByIdeve($ideve) {
+
+
+    public function ajouterPhotoEve($ideve, $photo) {
+        $con = connexionPDO();
+        $data = [
+            ':ideve' => $ideve,
+            ':lienphoto' => $photo
+        ];
+
+        $sql = "INSERT INTO photos (ideve, lienphoto) VALUES (:ideve, :lienphoto);";
+        $stmm = $con->prepare($sql);
+
+        if ($stmm->execute($data)) {
+            echo "Photo insérée";
+            return true;
+        } else {
+            echo $stmm->errorInfo();
+            return false;
+        }
+    }
+
+
+
+    // Méthode pour récupérer toutes les photos d'une cavalerie par ideve
+    public function getPhotosByIdEve($ideve) {
         $con = connexionPDO();
         $sql = "SELECT idphotos, lienphoto FROM photos WHERE ideve = :ideve";
         $data = [':ideve' => $ideve];
@@ -128,40 +175,18 @@ class Evenements {
         return $resultat;
     }
 
-    public function getSinglePhotoByIdeve($ideve) {
-        $con = connexionPDO();
-        $sql = "SELECT lienphoto FROM photos WHERE ideve = :ideve LIMIT 1";
-        $data = [':ideve' => $ideve];
-        $executesql = $con->prepare($sql);
-        $executesql->execute($data);
-        $resultat = $executesql->fetch();
-        return $resultat ? $resultat['lienphoto'] : 0;
-    }
 
-    public function ajouterPhoto($ideve, $photo) {
-    $con = connexionPDO();
-    $data = [
-        ':ideve' => $ideve,
-        ':lienphoto' => $photo,
-        ':numsire' => null // Assurez-vous que numsire est NULL pour les photos des événements
-    ];
-    $sql = "INSERT INTO photos (ideve, lienphoto, numsire) VALUES (:ideve, :lienphoto, :numsire);";
-    $stmm = $con->prepare($sql);
-    if ($stmm->execute($data)) {
-        echo "Photo insérée";
-        return true;
-    } else {
-        echo $stmm->errorInfo();
-        return false;
-    }
-}
-
-
+    
+    // Méthode pour supprimer une photo spécifique de la table photos
     public function supprimerPhoto($idphotos) {
         $con = connexionPDO();
-        $data = [':idphotos' => $idphotos];
+        $data = [
+            ':idphotos' => $idphotos
+        ];
+
         $sql = "DELETE FROM photos WHERE idphotos = :idphotos";
         $stmm = $con->prepare($sql);
+
         if ($stmm->execute($data)) {
             echo "Photo supprimée";
             return true;
